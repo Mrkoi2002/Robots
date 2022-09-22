71f4f3c236c0aeeb0b2c746dc98e020ef5dcac87
Nguyễn Phi Thắng <113253038+Mrkoi2002@users.noreply.github.com>
2022-09-22 23:08:14 +0700

    Add files via upload

---
+++ Hoa6canh.m
@@ -0,0 +1,21 @@
+l1=5;l2=5;
+syms t t1 t2 pi
+pi=3,14
+for t=0:0.01:12*pi
+    X = 10*(1+sin(6*t))*cos(t);
+    Y = 10*(1+sin(6*t))*sin(t);
+    C2=(X^2+Y^2-l1^2-l2^2)/(2*l1*l2)
+    S2=sqrt(abs(1-C2^2));
+    t2=atan2(S2,C2);
+    C1=(l1+l2*C2)*X+l2*S2*Y;
+    S1=(l1+l2*C2)*Y-l2*S2*X;
+    t1=atan2(S1,C1);
+    Px=l1*cos(t1)+l2*cos(t1+t2);
+    Py=l1*sin(t1)+l2*sin(t1+t2);
+   
+    subplot(2,1,1);
+    plot(Px,Py,'*');
+    xlabel('x(cm)');
+    ylabel('y(cm)');pause(0.1);
+    hold on
+end
